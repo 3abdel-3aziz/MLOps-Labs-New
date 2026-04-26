4379b3edf7fd19963b795d3486d416729730125d
Abdelaziz <ahmed2025.mohamed2000@gmail.com>
2026-04-26 19:12:21 +0300

    Integrate MLflow tracking in train.py

--- src/stages/train.py
+++ src/stages/train.py
@@ -4,33 +4,45 @@ from omegaconf import DictConfig
 from xgboost import XGBClassifier
 import joblib
 import os
+import mlflow
+import mlflow.xgboost  
+import dagshub
 
+
+dagshub.init(repo_owner='ahmed2025.mohamed2000', repo_name='MLOps-Labs-New', mlflow=True)
 @hydra.main(config_path="../../", config_name="config", version_base="1.2")
 def train(cfg: DictConfig):
-    train_df = pd.read_csv("data/processed/train_processed.csv")
-    
-    TARGET = cfg.params.target
-    X_train = train_df.drop(columns=[TARGET])
-    y_train = train_df[TARGET]
+    with mlflow.start_run():
+        train_df = pd.read_csv("data/processed/train_processed.csv")
+        
+        TARGET = cfg.params.target
+        X_train = train_df.drop(columns=[TARGET])
+        y_train = train_df[TARGET]
 
-    model = XGBClassifier(
-        n_estimators=cfg.model_params.n_estimators,
-        learning_rate=cfg.model_params.learning_rate,
-        max_depth=cfg.model_params.max_depth,
-        subsample=cfg.model_params.subsample,
-        colsample_bytree=cfg.model_params.colsample_bytree,
-        random_state=cfg.params.random_state,
-        eval_metric='logloss'
-    )
-    
-    print("Training the model via Hydra parameters...")
-    model.fit(X_train, y_train)
+        mlflow.log_params(cfg.model_params)
+        mlflow.log_param("target" , TARGET)
 
-    os.makedirs("models", exist_ok=True)
-    model_path = "models/model.joblib" 
-    joblib.dump(model, model_path)
-    
-    print(f" Model trained and saved to: {model_path}")
+
+        model = XGBClassifier(
+            n_estimators=cfg.model_params.n_estimators,
+            learning_rate=cfg.model_params.learning_rate,
+            max_depth=cfg.model_params.max_depth,
+            subsample=cfg.model_params.subsample,
+            colsample_bytree=cfg.model_params.colsample_bytree,
+            random_state=cfg.params.random_state,
+            eval_metric='logloss'
+        )
+        
+        print("Training the model via Hydra parameters...")
+        model.fit(X_train, y_train)
+
+        mlflow.xgboost.log_model(model, "xgboost_model")
+
+        os.makedirs("models", exist_ok=True)
+        model_path = "models/model.joblib" 
+        joblib.dump(model, model_path)
+        
+        print(f" Model trained and saved to: {model_path}")
 
 if __name__ == "__main__":
     train()
